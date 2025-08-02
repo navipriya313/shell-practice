541d5b8fde4f6edd5dceb02c0ac862d07bb3317f
priyanka <info@joindevops.com>
2025-08-02 11:50:33 +0530

    hello world started

--- 06-data-type.sh
+++ 06-data-type.sh
@@ -6,6 +6,6 @@ NUMBER2=100
 
 TIMESTAMP=$(date)
 echo "Script executed at: $TIMESTAMP"
-SUB=$(($NUMBER1-$NUMBER2))
+MUL=$(($NUMBER1*$NUMBER2))
 
-echo "SUB of $NUMBER1 and $NUMBER2 is: $SUB"
+echo "MUL of $NUMBER1 and $NUMBER2 is: $MUL"
